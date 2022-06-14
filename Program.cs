ea502efca649ad3543a8054c5bf8d6d48e4fc8a2
Gazeba <sipion46@mail.ru>
2022-06-14 22:10:48 +0300

    использовал FirstOrDefault

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var collection = Console.ReadLine().Split(new char[] { ' ' }).Select(it => int.Parse(it)).ToArray();
-var odd = from c in collection
-          where c % 2 == 0
-          select c;
-if (odd.Count() != 0)
+var odd = collection.FirstOrDefault(c => c % 2 == 0, 3);
+if (odd != 3)
 {
-    foreach (int i in odd)
-    {
-        Console.WriteLine(i);
-        break;
-    }
+    Console.WriteLine(odd);
 }
 else Console.WriteLine("четных чисел не имеется или список пуст");
