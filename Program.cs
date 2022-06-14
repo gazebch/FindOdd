1a9e0a880d21c666eb561aa69280a3d6b5b661f9
Gazeba <sipion46@mail.ru>
2022-06-14 20:30:00 +0300

    Вывод первого четного через foreach

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var collection = Console.ReadLine().Split(new char[] { ' ' }).Select(it => int.Parse(it)).ToArray();
+var odd = from c in collection
+          where c % 2 == 0
+          select c;
+if (odd.Count() != 0)
+{
+    foreach (int i in odd)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+}
+else Console.WriteLine("четных чисел не имеется или список пуст");
